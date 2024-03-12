cfcf98b38d568da3f74a69dc998f81d36eb27123
PauloHeleno <helenop16@gmail.com>
2024-03-12 15:52:46 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/PauloHeleno/course/config/Instantiation.java
+++ src/main/java/com/PauloHeleno/course/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.PauloHeleno.course.domain.Post;
 import com.PauloHeleno.course.domain.User;
 import com.PauloHeleno.course.dto.AuthorDTO;
+import com.PauloHeleno.course.dto.CommentDTO;
 import com.PauloHeleno.course.repository.PostRepository;
 import com.PauloHeleno.course.repository.UserRepository;
 
@@ -41,6 +42,12 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
